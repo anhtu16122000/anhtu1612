3d3a18a4878b2dd6d8cec72b38c0eeb1130d4aea
anhtu16122000 <46514076+anhtu16122000@users.noreply.github.com>
2019-01-09 13:15:29 +0700

    Update and rename .gitignore to .cpp

---
+++ .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+ 	int n;
+ 	int s;
+ 	double laisuat;
+ 	double year;
+  cout<<"nhap vao so tien ban dau";
+  cin>>n;
+  cout<<"nhap vao so tien ky vong";
+  cin>>s;
+  cout<<"nhap vao lai suat hang nam";
+  cin>>x;
+  year=(s-n)/laisuat;
+  cout<<"so nam de duoc so tien ky vong la"<<year;
+ return 0; 
+ }
